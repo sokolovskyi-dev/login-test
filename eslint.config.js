f3180c47d0905f75dc99338e5d0392d3438c8881
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2026-03-18 17:10:44 +0200

    feat: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,90 @@
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+
+export default defineConfig([
+  globalIgnores(['dist', 'build', 'node_modules', 'coverage']),
+
+  {
+    files: ['**/*.{js,jsx}'],
+
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      globals: {
+        ...globals.browser,
+      },
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+
+    rules: {
+      /*
+      -------------------------
+      Base JS hygiene
+      -------------------------
+      */
+
+      'no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^[A-Z_]',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 1,
+          maxEOF: 0,
+          maxBOF: 0,
+        },
+      ],
+
+      'eol-last': ['error', 'always'],
+
+      /*
+      -------------------------
+      Import sorting
+      -------------------------
+      */
+
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react$', '^react-dom$'],
+            ['^@?\\w'],
+            ['^@/'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?!/?$)', '^\\./?$'],
+            ['^.+\\.s?css$'],
+            ['^.+\\.(png|jpe?g|svg|gif|webp)$'],
+          ],
+        },
+      ],
+
+      'simple-import-sort/exports': 'error',
+    },
+  },
+]);
---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://lpj-tasker.herokuapp.com';
