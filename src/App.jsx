ad2e46c8bea0a7e694e58e18b650309e89376483
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2026-03-19 16:23:18 +0200

    529859852985

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 // import { Button } from '@/components/ui/button';
 
+import { CircleFadingArrowUpIcon } from 'lucide-react';
+
 import { Button } from './components/ui/button';
 
 function App() {
   return (
     <div>
       App
-      <Button variant="secondary">Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+      <Button>
+        <CircleFadingArrowUpIcon /> Click me
+      </Button>
     </div>
   );
 }
