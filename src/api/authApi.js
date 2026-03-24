f980321e2206ab50f6de75fd2d8c8cc8d92bdd46
DevMasterX <sokol080288@gmail.com>
2026-03-25 01:04:52 +0200

    124141

---
+++ src/api/authApi.js
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://connections-api.goit.global' }),
+  //   tagTypes: ['Posts'],
+  endpoints: (build) => ({
+    signUp: build.mutation({
+      query(body) {
+        return {
+          url: `/users/signup`,
+          method: 'POST',
+          body,
+        };
+      },
+    }),
+
+    signIn: build.mutation({
+      query(body) {
+        return {
+          url: `/users/login`,
+          method: 'POST',
+          body,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useSignUpMutation, useSignInMutation } = authApi;
